9f53c826f8f9c81768b6e1db67e04edb27057062
Daniel Pakosz <Better@DANIEL-LAPTOP>
2021-12-13 17:03:31 +0100

    Show tenent data

--- src/main/java/pl/polsl/confrooms/controller/UserController.java
+++ src/main/java/pl/polsl/confrooms/controller/UserController.java
@@ -9,16 +9,26 @@ import pl.polsl.confrooms.model.User.UserService;
 
 //CONTROLLER ODPOWIEDZIALNY ZA REJESTRACJE UZYTKOWNIKA
 @Controller
+@RequestMapping("/userPanel")
 @AllArgsConstructor
 public class UserController {
 
     private UserService userService;
 
-    @GetMapping("userPanel")
+    @GetMapping
     @ResponseBody
     public ModelAndView loggedUser()
     {
         ModelAndView response = new ModelAndView("tenant_panel/tenant_panel");
+        response.addObject("loggedUser",userService.getPanelData());
+        return response;
+    }
+
+    @GetMapping("/data")
+    @ResponseBody
+    public ModelAndView getData()
+    {
+        ModelAndView response = new ModelAndView("tenant_panel/tenant_data");
         response.addObject("loggedUser",userService.getPanelData());
         return response;
     }
